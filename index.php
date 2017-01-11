34b53f4bda4cdcdac6836fe94c85e6d69378bf0d
FreezyGel <maximeschick97@gmail.com>
2017-01-11 10:07:48 +0100

    Ajout du HTML

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<?php
-  /*
-  //Connexion à la base de données
-  try{
-
-  }
-  catch(Excepetion $e)
-  {
-    die('Erreur : '.$e->getMessage());
-  }
-  */
-?>
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <title>Logiciel PHP</title>
+    </head>
+    <body>
+      <h1>Page HTML et PHP</h1>
+      <?php
+        /*
+        //Connexion à la base de données
+        try{
+          $bdd = new PDO('mysql:host=localhost;dbname=test;chaset=utf8', 'Username', 'Password');
+        }
+        catch(Excepetion $e)
+        {
+          die('Erreur : '.$e->getMessage());
+        }
+        */
+      ?>
+    </body>
+  </html>
